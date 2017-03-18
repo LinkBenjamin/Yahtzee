ae87979a1faa74901a2217ba9ffc7e027ffe5325
Ben Link <LinkBenjamin@users.noreply.github.com>
2017-03-18 19:42:27 -0400

    Update Thing.java

--- Yahtzee/src/com/benlink/driver/Thing.java
+++ Yahtzee/src/com/benlink/driver/Thing.java
@@ -9,24 +9,40 @@ public class Thing {
         
         Scanner keyboard = new Scanner(System.in);
         boolean QUIT = false;
+        YahtzeeGame game;
+        Player winner;
         
         do {
+            game = new YahtzeeGame(keyboard);
+            
             //Initialize Player List
+            game.initialize();
+            
             //Play a Game
+            game.play();
+            
             //Compare Scorecards
+            winner = game.determineWinner();
+            
             //Announce Winner
+            announceWinner(winner);
             
             //Ask if we should play again
             QUIT = !(askUserBoolean("Well, that was a fun game.  Would you like to play again? (y/n): ",keyboard));
         } while (!QUIT);
         
-        Cup cup = new Cup();
-        
-        cup.play(true, true, true, true, true);
-        cup.display();
+        keyboard.close();
     }
     
-    
+    /**
+     * Announce the winning player and show their scorecard
+     *
+     * @param winner  The Player object of the winning player
+     * 
+     */
+    private static void announceWinner(Player winner){
+        System.out.println("Haven't written it yet");
+    }
     /**
      * Ask the user a question.  Get a yes or no answer.
      * 
